ef1d26279daf055f65ea322b99afa533c9132e2b
Kim Marshalls <dev.kimmarshalls@gmail.com>
2026-06-01 16:22:41 +0300

    Fix service dropdown links on root pages

--- fixbytes.py
+++ fixbytes.py
@@ -1,65 +1,29 @@
-import os, glob, re
+import os, glob
 
 BASE = os.getcwd()
 
-def fix_file(filepath, is_service):
-    prefix = b'../assets/images/' if is_service else b'assets/images/'
+# Fix broken service links in about.html and other root pages
+# They incorrectly point to index.html#services
+replacements = [
+    (b'href="index.html#services"', b'href="services/index.html"'),
+]
 
+files = sorted(glob.glob(os.path.join(BASE, '*.html')))
+
+total = 0
+for filepath in files:
     with open(filepath, 'rb') as f:
         content = f.read()
     original = content
-
-    # Replace desktop navbar logo (handles any whitespace between elements)
-    content = re.sub(
-        b'<div class="nav-logo-icon"[^>]*></div>\\s*'
-        b'<div class="nav-logo-text">\\s*'
-        b'<span class="nav-logo-wordmark"[^>]*>Brezoid</span>\\s*'
-        b'<span class="nav-logo-tagline">[^<]*</span>\\s*'
-        b'</div>',
-        b'<img src="' + prefix + b'Brezoid-logo-light.svg" alt="Brezoid Global Networks Limited" '
-        b'class="nav-logo-img nav-logo-img-white" width="160" height="38" />'
-        b'<img src="' + prefix + b'brezoid-logo.svg" alt="" aria-hidden="true" '
-        b'class="nav-logo-img nav-logo-img-color" width="160" height="38" />',
-        content
-    )
-
-    # Replace mobile menu logo (has inline style on wordmark)
-    content = re.sub(
-        b'<div class="nav-logo-icon"[^>]*></div>\\s*'
-        b'<div class="nav-logo-text">\\s*'
-        b'<span class="nav-logo-wordmark" style="[^"]*">Brezoid</span>\\s*'
-        b'<span class="nav-logo-tagline">[^<]*</span>\\s*'
-        b'</div>',
-        b'<img src="' + prefix + b'brezoid-logo.svg" alt="Brezoid Global Networks Limited" '
-        b'class="nav-logo-img" width="150" height="36" />',
-        content
-    )
-
-    # Replace footer logo
-    content = re.sub(
-        b'<div class="nav-logo-icon"[^>]*></div>\\s*'
-        b'<div class="nav-logo-text">\\s*'
-        b'<span class="nav-logo-wordmark"[^>]*>Brezoid</span>\\s*'
-        b'<span class="nav-logo-tagline">[^<]*</span>\\s*'
-        b'</div>',
-        b'<img src="' + prefix + b'Brezoid-logo-light.svg" alt="Brezoid Global Networks Limited" '
-        b'class="footer-logo-img" width="150" height="34" />',
-        content
-    )
-
+    for find, replace in replacements:
+        content = content.replace(find, replace)
     if content != original:
         with open(filepath, 'wb') as f:
             f.write(content)
         print('Fixed: ' + os.path.relpath(filepath, BASE))
+        total += 1
     else:
         print('OK:    ' + os.path.relpath(filepath, BASE))
 
-root_files = sorted(glob.glob(os.path.join(BASE, '*.html')))
-svc_files  = sorted(glob.glob(os.path.join(BASE, 'services', '*.html')))
-
-for f in root_files:
-    fix_file(f, is_service=False)
-for f in svc_files:
-    fix_file(f, is_service=True)
-
-print('\nDone.')
+print('')
+print('Done. ' + str(total) + ' files updated.')
